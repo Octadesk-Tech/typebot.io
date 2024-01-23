5e394fef83822f20d5521c8e31342aea2a24efb6
Lucas Cordeiro Araujo <lucascordeiroaraujo@gmail.com>
2024-01-23 13:38:21 -0300

    chore: revert last commit

--- apps/builder/components/shared/Graph/Nodes/StepNode/TextBubbleEditor/TextBubbleEditor.tsx
+++ apps/builder/components/shared/Graph/Nodes/StepNode/TextBubbleEditor/TextBubbleEditor.tsx
@@ -69,13 +69,6 @@ export const TextBubbleEditor = ({
       nodes: v,
     }).replace(/<[^/>]*>\s*<\/[^>]*>/g, '')
 
-    if (!html) {
-      return {
-        html,
-        richText: v,
-      }
-    }
-
     return {
       html,
       richText: v,
