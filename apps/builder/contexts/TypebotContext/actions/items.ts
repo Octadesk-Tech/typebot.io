d6d68dad397fdde5221d75d77fd14da4ef83b1cd
Daniel Ferreira <daniel.ferreira@octadesk.com>
2024-03-21 14:27:19 -0300

    fix: merge fix

--- apps/builder/contexts/TypebotContext/actions/items.ts
+++ apps/builder/contexts/TypebotContext/actions/items.ts
@@ -33,18 +33,12 @@ const itemsAction = (setTypebot: SetTypebot): ItemsActions => ({
       produce(typebot, (typebot) => {
         const step = typebot.blocks[blockIndex].steps[stepIndex]
         if (
-<<<<<<< HEAD
           step.type !== InputStepType.CHOICE &&
           step.type !== OctaWabaStepType.WHATSAPP_OPTIONS_LIST &&
-          step.type !== OctaWabaStepType.WHATSAPP_BUTTONS_LIST
+          step.type !== OctaWabaStepType.WHATSAPP_BUTTONS_LIST &&
+          step.type !== WOZStepType.ASSIGN
         )
           return
-=======
-          step.type !== InputStepType.CHOICE && 
-          step.type !== OctaWabaStepType.WHATSAPP_OPTIONS_LIST && 
-          step.type !== OctaWabaStepType.WHATSAPP_BUTTONS_LIST &&
-          step.type !== WOZStepType.ASSIGN) return
->>>>>>> main
         const newItem = {
           ...item,
           stepId: step.id,
